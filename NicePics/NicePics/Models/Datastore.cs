a1aeeef88948e5cdeb3fef604d5042e73d712928
aspramsh <aspramshadyan@gmail.com>
2017-05-21 01:38:52 +0400

    Add files via upload

---
+++ NicePics/NicePics/Models/Datastore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NicePics.Models
+{
+    /// <summary>
+    /// A static class for keeping added photos and getting photos by name
+    /// </summary>
+    public static class Datastore
+    {
+        /// <summary>
+        /// A list for keeping photos
+        /// </summary>
+        public static List<Image> Images = new List<Image>();
+        /// <summary>
+        /// A method for adding photos to the Images list
+        /// </summary>
+        /// <param name="image"></param>
+        public static void AddImage(Image image)
+        {
+            Images.Add(image);
+        }
+        /// <summary>
+        /// A method for getting a photo from images list
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <returns></returns>
+        public static Image GetImage(string imageName)
+        {
+            List<Image> list = Images.Where(a => a.ImageName.Equals(imageName)).ToList();
+            if (Images.Count() > 0 && list.Count() > 0)
+                return list[0];
+            else
+                return null;
+        }
+    }
+}
---
+++ NicePics/NicePics/Models/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace NicePics.Models
+{
+    /// <summary>
+    /// An image class for image objects in the server part
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        /// A string property for storing image name
+        /// </summary>
+        public string ImageName { get; set; }
+        /// <summary>
+        /// A property for storing image converted to string
+        /// </summary>
+        public byte[] ImageData { get; set; }
+    }
+}
